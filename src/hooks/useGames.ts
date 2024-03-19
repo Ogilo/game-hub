40acc3cd6c2688f32a6a9d5f255ed77697e49135
Ozren <ozrenpetrovic1@gmail.com>
2024-03-19 18:19:43 +0100

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,7 +6,6 @@ export interface Platform {
   id: number;
   name: string;
   slug: string;
-  
 }
 
 export interface Game {
@@ -15,6 +14,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = ( gameQuery: GameQuery ) =>
